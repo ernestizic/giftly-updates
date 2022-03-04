8411fa0312e20583a428d3dcd55f328e827494f9
Lawrence Ughonu <lawrenceughonu@gmail.com>
2022-03-04 10:26:39 +0100

    Making landing responsive

--- src/components/landing/Home.jsx
+++ src/components/landing/Home.jsx
@@ -7,44 +7,96 @@ import hm_hero_1sm from "../../assets/images/hm_hero_1sm.png";
 const Wrapper = styled.div`
 	padding: 0 12rem;
 
-.header{
-    display:flex;
-}
-.text{
-    width: 38.19%;
-}
+	@media (max-width: 1260px) {
+		padding: 0 9rem;
+		.title-plus {
+			font-size: 9rem;
+		}
+	}
+	@media (max-width: 1110px) {
+		padding: 0 7.7rem;
+		.title-plus {
+			font-size: 8rem;
+		}
+	}
+	@media (max-width: 1000px) {
+		padding: 0 6rem;
+		.title-plus {
+			font-size: 7rem;
+		}
+	}
+	@media (max-width: 860px) {
+		padding: 0 4rem;
+		.title-plus {
+			font-size: 6.6rem;
+		}
+	}
+	@media (max-width: 760px) {
+		padding: 0;
+		.title-plus {
+			font-size: 6.6rem;
+		}
+	}
 
-.title-plus{
-    color:var(--white);
-    .wish{
-        color:var(--accent_1-light);
-    }
-    .friends{
-        color:var(--primary-main);
-    }
-}
+	.header {
+		display: flex;
+		@media (max-width: 760px) {
+			flex-direction: column;
+			h1 {
+				text-align: center;
+				width: 70%;
+				margin: auto;
+				font-size: 6rem;
+				line-height: 5.8rem;
+			}
+		}
+		@media (max-width: 530px) {
+			flex-direction: column;
+			h1 {
+				text-align: center;
+				width: 70%;
+				margin: auto;
+				font-size: 4.5rem;
+				line-height: 2.8rem;
+			}
+		}
+	}
+	.text {
+		/* width: 38.19%; */
+		flex: 1;
+	}
 
-.subtitle-2{
-    color: white;
-}
+	.title-plus {
+		color: var(--white);
+		.wish {
+			color: var(--accent_1-light);
+		}
+		.friends {
+			color: var(--primary-main);
+		}
+	}
+
+	.subtitle-2 {
+		color: white;
+	}
 
 	.btn {
 		width: 23.2rem;
 	}
 
-    .imgWrapper{
-        width: 64rem;
-        /* flex: 1; */
+	.imgWrapper {
+		/* width: 64rem; */
+		flex: 1;
 
-        img, source{
-            width:100%;
-
-        }
-        /* picture{
+		img,
+		source {
+			width: 100%;
+		}
+		/* picture{
             border-color: red;
 
         } */
-    }
+	}
 `;
 
 export const Home = () => {
@@ -52,17 +104,17 @@ export const Home = () => {
 		<Wrapper>
 			<div className="header">
 				<div className="text">
-					<p className="title-plus">
+					<h1 className="title-plus">
 						Shoot a <span className="wish">Wish</span> and Share With{" "}
 						<span className="friends">Friends!</span>
-					</p>
+					</h1>
 					<p className="subtitle-2">
 						Create a wish list in minutes and share to your friends on your
 						favourite platforms!
 					</p>
 					<Button className="btn" text="Create a Wish list for Free" />
 				</div>
-				<div  className="imgWrapper">
+				<div className="imgWrapper">
 					{/* Responsive image using art direction and resolution switching
                     Could not use Density switching because we do not have images of different image density */}
 					<picture>
