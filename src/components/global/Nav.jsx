aacb6a54f9cfd588b536ff3be756e517a51615af
Azerii <ogidi58@gmail.com>
2022-04-05 17:03:15 +0100

    fix nav width

--- src/components/global/Nav.jsx
+++ src/components/global/Nav.jsx
@@ -17,7 +17,7 @@ import { Initials } from "components/user/WishListsStyles";
 const Wrapper = styled.nav`
   height: 9.6rem;
   padding: 0 12rem;
-  max-width: 1440px;
+  // max-width: 1440px;
   margin: auto;
   background-color: ${(props) =>
     props.wt ? "white" : " var(--title-active);"};
@@ -36,6 +36,7 @@ const Wrapper = styled.nav`
     margin-left: 4.8rem;
     margin-right: auto;
     display: block;
+
     @media (max-width: 768px) {
       /* width: auto */
       .input {
@@ -287,29 +288,5 @@ const Nav = ({ wt }) => {
     </Wrapper>
   );
 };
-
-//   & > div > div {
-//     padding-left: 3.667rem;
-//     @media (max-width: 768px) {
-//       padding: 0;
-//     }
-//   }
-// }
-// .search {
-//   position: absolute;
-//   z-index: 6;
-//   left: 2.4rem;
-//   top: 1.6rem;
-//   @media (max-width: 768px) {
-//     position: sticky;
-//     top: 4.1rem;
-//     left: 23%;
-//     filter: var(--filter-white);
-//   }
-// }
-// & > button:last-child {
-//   margin-left: 2.4rem;
-// }
-// `;
 
 export default Nav;
